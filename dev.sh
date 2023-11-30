8d3a09b1f731432d2d4763ff0e1a3b0d24c9368e
Fabian <50114802+fabalexsie@users.noreply.github.com>
2023-11-30 23:45:49 +0100

    Fix: development docker

--- dev.sh
+++ dev.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # docker-compose-personal.yml is local only overriding spotify public and secret for development
-docker compose -f docker-compose-dev.yml up --build --attach app --attach web
+docker compose -f docker-compose-dev.yml up --build --attach server --attach web
