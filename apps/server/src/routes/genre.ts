09e5fdfa396cbebe0c597449c2bba3238ee4d74f
Fabian <50114802+fabalexsie@users.noreply.github.com>
2025-05-04 19:24:03 +0200

    fix: missed validating from merge

--- apps/server/src/routes/genre.ts
+++ apps/server/src/routes/genre.ts
@@ -1,8 +1,6 @@
 import { Router } from "express";
 import { z } from "zod";
-import { logger } from "../tools/logger";
-import { isLoggedOrGuest, validating } from "../tools/middleware";
-import { TypedPayload } from "../tools/types";
+import { isLoggedOrGuest, validate } from "../tools/middleware";
 import { getGenre } from "../database";
 
 export const router = Router();
@@ -11,72 +9,46 @@ const getGenreSchema = z.object({
   genreName: z.string(),
 });
 
-router.get(
-  "/:genreName",
-  validating(getGenreSchema, "params"),
-  isLoggedOrGuest,
-  async (req, res) => {
-    try {
-      const { genreName } = req.params as TypedPayload<typeof getGenreSchema>;
-      const genre = await getGenre(genreName);
-      if (!genre) {
-        res.status(404).end();
-        return;
-      }
-      res.status(200).send({
-        ...genre,
-      });
-    } catch (e) {
-      logger.error(e);
-      res.status(500).end();
-    }
-  },
-);
+router.get("/:genreName", isLoggedOrGuest, async (req, res) => {
+  const { genreName } = validate(req.params, getGenreSchema);
+  const genre = await getGenre(genreName);
+  if (!genre) {
+    res.status(404).end();
+    return;
+  }
+  res.status(200).send({
+    ...genre,
+  });
+});
 
 const getGenreStats = z.object({
   genreName: z.string(),
 });
 
-router.get(
-  "/:genreName/stats",
-  validating(getGenreStats, "params"),
-  isLoggedOrGuest,
-  async (req, res) => {
-    try {
-      const { genreName } = req.params as TypedPayload<typeof getGenreStats>;
-      const [genre] = await getGenre(genreName);
-      if (!genre) {
-        res.status(404).end();
-        return;
-      }
-      res.status(200).send({
-        genre,
-      });
-    } catch (e) {
-      logger.error(e);
-      res.status(500).end();
-    }
-  },
-);
+router.get("/:genreName/stats", isLoggedOrGuest, async (req, res) => {
+  const { genreName } = validate(req.params, getGenreStats);
+  const [genre] = await getGenre(genreName);
+  if (!genre) {
+    res.status(404).end();
+    return;
+  }
+  res.status(200).send({
+    genre,
+  });
+});
 
 // router.get(
 //   "/:id/rank",
-//   validating(getAlbumStats, "params"),
 //   isLoggedOrGuest,
 //   async (req, res) => {
-//     try {
-//       const { user } = req as LoggedRequest;
-//       const { id } = req.params as TypedPayload<typeof getAlbumStats>;
-//       const [album] = await getAlbums([id]);
-//       if (!album) {
-//         res.status(404).end();
-//         return;
-//       }
-//       const rank = await getRankOf(ItemType.album, user, id);
-//       res.status(200).send(rank);
-//     } catch (e) {
-//       logger.error(e);
-//       res.status(500).end();
-//     }
+//      const { user } = req as LoggedRequest;
+//      const { id } = validate(req.params, getAlbumStats);
+//      const [album] = await getAlbums([id]);
+//      if (!album) {
+//        res.status(404).end();
+//        return;
+//      }
+//      const rank = await getRankOf(ItemType.album, user, id);
+//      res.status(200).send(rank);
 //   },
 // );
