9d891291dfeaf3d8ca6b2e7d37507f737275d7ec
Fabian <50114802+fabalexsie@users.noreply.github.com>
2025-11-14 23:37:07 +0100

    sort artists in genre overview by name

--- apps/server/src/database/queries/genre.ts
+++ apps/server/src/database/queries/genre.ts
@@ -3,6 +3,7 @@ import { ArtistModel } from "../Models";
 export const getGenre = (genreName: string) =>
   ArtistModel.aggregate([
     { $match: { genres: { $in: [genreName] } } },
+    { $sort: { name: 1, popularity: -1, _id: 1 } },
     { $group: { _id: null, artists: { $push: "$$ROOT" } } },
     { $addFields: { name: genreName } },
   ]);
