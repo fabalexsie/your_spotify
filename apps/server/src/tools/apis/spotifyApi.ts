e829a65d7b975bb7d0722d0264cfb82555bd89c2
Fabian <50114802+fabalexsie@users.noreply.github.com>
2026-05-30 11:47:51 +0200

    fix: add to queue

--- apps/server/src/tools/apis/spotifyApi.ts
+++ apps/server/src/tools/apis/spotifyApi.ts
@@ -76,10 +76,8 @@ export class SpotifyAPI {
   public async addToQueue(trackUri: string) {
     await this.checkToken();
     return this.client.instance.post(
-      "https://api.spotify.com/v1/me/player/queue",
-      {
-        uri: trackUri,
-      },
+      `https://api.spotify.com/v1/me/player/queue?uri=${encodeURIComponent(trackUri)}`,
+      {},
     );
   }
 
